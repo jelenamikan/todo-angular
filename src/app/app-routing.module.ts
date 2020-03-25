376f104a2a6c0b8b205d4a8eb8d8e6b62812c5a0
Jelena <jelena.mikan95@gmail.com>
2020-03-25 20:30:28 +0100

    add welcome, login and error components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path:'welcome/:name', component: WelcomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username = 'jecapereca'
+  password = ''
+  errorMessage='Invalid credentials'
+  invalidLogin = false
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  handleLogin(){
+    if (this.username === 'jecapereca' && this.password === 'dummy'){
+      this.router.navigate(['welcome', this.username])
+      this.invalidLogin = false
+    } else {
+      this.invalidLogin = true
+    }
+  }
+
+}
