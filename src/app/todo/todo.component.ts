0988f9d823f27385829c949003c738ba53f5c1fc
Jelena <jelena.mikan95@gmail.com>
2020-04-01 22:32:13 +0200

    add update todo

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoDataService } from '../service/data/todo-data.service';
 import { Todo } from '../list-todos/list-todos.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo',
@@ -13,7 +13,7 @@ export class TodoComponent implements OnInit {
   id: number
   todo: Todo
 
-  constructor(private todoService: TodoDataService, private route: ActivatedRoute) { }
+  constructor(private todoService: TodoDataService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
@@ -25,6 +25,13 @@ export class TodoComponent implements OnInit {
     )
   }
 
-  saveTodo(){}
+  saveTodo(){
+    this.todoService.updateTodo('jecapereca', this.id, this.todo).subscribe(
+      data => {
+        console.log(data)
+        this.router.navigate(['todos'])
+      }
+    )
+  }
 
 }
